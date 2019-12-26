a8ed6bd430ab94a982b7f1b69fd83199de7ef207
Carlos Victor <carlosvictortecprof@gmail.com>
2019-12-25 21:30:17 -0300

    Adicionado algumas validacoes

--- forca.py
+++ forca.py
@@ -13,27 +13,31 @@ while erros <= 6:
     desenho.imprimir_desenhos(erros), print(
         f"Dica: {dica}, Tamanho da palavra: {desenho.func_tam_palavra(str(check_palavra))}")
     letra = input("Digite uma Letra para acertar a palavra: ")
-    try:
-        tentativa = letras_chamadas.index(letra)
-        print("A letra já foi chamada tente novamente")
-    except:
+    if len(letra) > 1:
+        print("Digite apenas uma letra")
+    else:
         try:
-            tentativa = list(palavra).index(letra)
-            copia_palavra = list(palavra)
-            for x in range(len(palavra)):
-                try:
-                    posicao = copia_palavra.index(letra)
-                    check_palavra[posicao] = letra
-                    copia_palavra[posicao] = '\n'
-                except:
-                    pass
-            letras_chamadas.append(letra)
+            tentativa = letras_chamadas.index(letra)
+            print("A letra já foi chamada tente novamente")
         except:
-            print("Está Letra não possui nesta palavra")
-            erros = erros + 1
-            print(erros)
-    if check_palavra == list(palavra):
-        print("Você Acertou a Palavra")
-        break
+            try:
+                tentativa = list(palavra).index(letra)
+                copia_palavra = list(palavra)
+                for x in range(len(palavra)):
+                    try:
+                        posicao = copia_palavra.index(letra)
+                        check_palavra[posicao] = letra
+                        copia_palavra[posicao] = '\n'
+                    except:
+                        pass
+                letras_chamadas.append(letra)
+            except:
+                print("Está Letra não possui nesta palavra")
+                erros = erros + 1
+        if check_palavra == list(palavra):
+            print(
+                f"Você Acertou a Palavra: {palavra}, você chamou essas letras {letras_chamadas}, você errou {erros} para acertar!")
+            break
 if erros >= 6:
-    print("Você Perdeu")
+    print(
+        f"Você Perdeu, a palavra era {palavra},você chamou essas letras {letras_chamadas}")
